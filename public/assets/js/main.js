4b913ef65c497fd0b679165d38826673797a74e8
NaiSaratxaga <nsaratxaga@gmail.com>
2022-04-16 01:40:24 +0200

    add fetch

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-console.log('>> Ready :)');
+const url = `https://randomuser.me/api/?results=10`;
+
+const list = document.querySelector('.js_list');
+
+function paintHTML(contacts) {
+  for (const contact of contacts) {
+    const li = `<li>
+    <div class="image">
+        <img src="${contact.picture.medium}" alt="contact picture" title="contact picture"/> 
+      </div>
+      <div class="name">
+        ${contact.name.first} ${contact.name.last} 
+      </div>
+      <div class="city">
+        ${contact.location.city} 
+      </div>
+      <div class="userName">
+        ${contact.login.username} 
+      </div>
+    </li>`;
+    list.innerHTML += li;
+  }
+}
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const allResult = data.results;
+    paintHTML(allResult);
+  });
 
 //# sourceMappingURL=main.js.map
